7a9d4f5ece728bba2dbb343ad8fe365186628b13
Abhinav Dasari <abhirishidasari@gmail.com>
2026-08-08 13:27:13 +0530

    change the debug state

--- app.py
+++ app.py
@@ -470,4 +470,4 @@ def server_error(e):
 # ─── Run ─────────────────────────────────────────────
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
